49734145371a99d206b387d51ad949842948cbba
kennymaina <victormurigi@outlook.com>
2019-01-31 15:39:13 +0300

    inpu and output addition

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -9,8 +9,8 @@ import {Quote} from '../quote'
 })
 export class QuoteDetailsComponent implements OnInit {
 
-  @Input() quote:Quote;
-  @Output() isComplete= new EventEmitter<boolean>();
+  // @Input() quote:Quote;
+  // @Output() isComplete= new EventEmitter<boolean>();
 
   quoteDelete(complete:boolean){
     this.isComplete.emit(complete);

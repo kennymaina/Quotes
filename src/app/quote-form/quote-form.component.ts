a5ca2109675aeb7391c5152c9853eb51e758125a
kennymaina <victormurigi@outlook.com>
2019-03-24 21:33:47 +0300

    src/app/quote-form/quote-form.component.ts

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -10,7 +10,7 @@ import { Quote } from '../quote';
 })
 export class QuoteFormComponent implements OnInit {
   newQuote =new Quote('',"","",new Date());
-  @Output() addQuote=new EventEmitter<Quote>();
+  // @Output() addQuote=new EventEmitter<Quote>();
     
     submitQuote(){
         this.addQuote.emit(this.newQuote);
